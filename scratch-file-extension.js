bcc711ca725c09211b1284d51ddd98b91b9fbf34
8odi <134816585+8odi@users.noreply.github.com>
2023-05-27 20:46:46 -0400

    Update scratch-file-extension.js

--- scratch-file-extension.js
+++ scratch-file-extension.js
@@ -1,46 +1,47 @@
-class ScratchFile {
-  constructor () {
-    this.fileInput = document.createElement("input");
-    this.fileInput.type = "file";
-    this.fileInput.accept = ".txt";
-    this.fileInput.style.display = "none";
-    this.fileInput.addEventListener("change", this.handleFileSelect.bind(this));
-    document.body.appendChild(this.fileInput);
-  }
-
-  getInfo () {
+const extensionCode = `
+class StrechExtension {
+  getInfo() {
     return {
-      "id": "File",
-      "name": "File",
-      "blocks": [
+      id: 'strech',
+      name: 'Strech',
+      blocks: [
         {
-          "opcode": "chooseFile",
-          "blockType": "reporter",
-          "text": "choose a .txt file",
-          "arguments": {}
-        }
-      ]
+          opcode: 'strechDance',
+          blockType: Scratch.BlockType.COMMAND,
+          text: 'dance',
+        },
+      ],
+      menus: {},
     };
   }
 
-  chooseFile () {
-    this.fileInput.click();
-    return new Promise(resolve => {
-      this.resolve = resolve;
-    });
-  }
+  strechDance() {
+strechDance() {
+  const sprite = this.runtime.getTargetForStage().sprite;
+  const originalSize = sprite.size;
+  const stretchFactor = 2; // Adjust the value as per your preference
 
-  handleFileSelect (event) {
-    const file = event.target.files[0];
-    if (file) {
-      const reader = new FileReader();
-      reader.onload = () => {
-        const content = reader.result;
-        this.resolve(file.name + "\n" + content);
-      };
-      reader.readAsText(file);
-    }
+  const intervalId = setInterval(() => {
+    sprite.size *= stretchFactor;
+    sprite.gotoXY(
+      sprite.x + Math.random() * 10 - 5,
+      sprite.y + Math.random() * 10 - 5
+    );
+  }, 10); // 10 milliseconds = 0.01 seconds
+
+  setTimeout(() => {
+    sprite.size = originalSize;
+    clearInterval(intervalId);
+  }, 5000); // Stop after 5 seconds (adjust the duration as needed)
+}
+
   }
 }
 
-Scratch.extensions.register(new ScratchFile());
+Scratch.extensions.register(new StrechExtension());
+`;
+
+// Inject the extension code into the SheepTester environment
+const scriptElement = document.createElement('script');
+scriptElement.textContent = extensionCode;
+document.head.appendChild(scriptElement);
